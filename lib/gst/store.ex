2308fdc96d0455096665a4fc570439ac33a15417
Martin Stannard <martin@stannard.id.au>
2016-02-01 23:07:56 +1100

    add server callbacks for put and get

--- lib/gst/store.ex
+++ lib/gst/store.ex
@@ -1,9 +1,20 @@
 defmodule Gst.Store do
   use GenServer
 
+  # server implementation
   def init(_) do
     IO.puts "Gst.Store init"
     { :ok, Map.new }
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    IO.puts "Gst.Store put"
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_call(:get, key}, _from, state) do
+    IO.puts "Gst.Store get"
+    {:reply, Map.get(state, key), state}
   end
 
 end
