9601b37b2a9585ee92b2fb04492ac428e9659677
Martin Stannard <martin@stannard.id.au>
2016-02-01 23:34:17 +1100

    Add clear implementation

--- lib/gst/store.ex
+++ lib/gst/store.ex
@@ -16,6 +16,9 @@ defmodule Gst.Store do
 
   def all(pid) do
     GenServer.call(pid, {:all})
+
+  def clear(pid) do
+    GenServer.cast(pid, {:clear})
   end
 
   # server callbacks
@@ -27,6 +30,11 @@ defmodule Gst.Store do
   def handle_cast({:put, key, value}, state) do
     IO.puts "Gst.Store put"
     {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_cast({:clear}, state) do
+    IO.puts "Gst.Store clear"
+    {:noreply, Map.new}
   end
 
   def handle_call({:get, key}, _from, state) do
