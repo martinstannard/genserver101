e9c420d5f11cb0df4406825b0d88f421cd94c741
Martin Stannard <martin@stannard.id.au>
2016-02-01 23:17:02 +1100

    add client interface functions

--- lib/gst/store.ex
+++ lib/gst/store.ex
@@ -1,6 +1,19 @@
 defmodule Gst.Store do
   use GenServer
 
+  # client interface
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
   # server callbacks
   def init(_) do
     IO.puts "Gst.Store init"
@@ -12,7 +25,7 @@ defmodule Gst.Store do
     {:noreply, Map.put(state, key, value)}
   end
 
-  def handle_call(:get, key}, _from, state) do
+  def handle_call({:get, key}, _from, state) do
     IO.puts "Gst.Store get"
     {:reply, Map.get(state, key), state}
   end
